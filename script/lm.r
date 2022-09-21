48ca529a459362886061a2d6b45defd81e75976f
Shenwei <692949389@QQ.com>
2022-09-21 15:05:35 +0800

    Create lm.r

---
+++ script/lm.r
@@ -0,0 +1,7 @@
+#! bin/bash/R
+
+args <- commandArgs(T)
+DATA <- read.table(args[1], header = TRUE, sep = "\t")
+model <- lm(formula = DATA[,3] ~ DATA[,2],data = DATA, na.action = na.omit)
+NAME <- paste(sep="", args[1], ".lst")
+write.table(coef(summary(model))[,4],file = NAME, quote = FALSE, sep = "\t")
