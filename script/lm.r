7c0dfce7e7ba2a5f261224fc9688d997653a39e0
Shenwei <692949389@QQ.com>
2022-09-28 09:31:54 +0800

    Update lm.r

--- script/lm.r
+++ script/lm.r
@@ -1,7 +1,8 @@
-#! bin/bash/R
+#! usr/bin/R
 
 args <- commandArgs(T)
 DATA <- read.table(args[1], header = TRUE, sep = "\t")
 model <- lm(formula = DATA[,3] ~ DATA[,2],data = DATA, na.action = na.omit)
 NAME <- paste(sep="", args[1], ".lst")
 write.table(coef(summary(model))[,4],file = NAME, quote = FALSE, sep = "\t")
+
